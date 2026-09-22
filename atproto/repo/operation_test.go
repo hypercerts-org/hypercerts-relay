ebe5aab52ee1a5fba0e615d6f6f7a7061d618f97
Evelyn Osman <ashex@chipnick.com>
2026-09-22 10:43:04 +0200

    fix: replace legacy IPFS blockstore dependency

--- atproto/repo/operation_test.go
+++ atproto/repo/operation_test.go
@@ -9,9 +9,9 @@ import (
 
 	"github.com/bluesky-social/indigo/atproto/repo/mst"
 
+	blockstore "github.com/ipfs/boxo/blockstore" // hypercerts: replace incompatible legacy go-ipfs-blockstore dependency
 	"github.com/ipfs/go-cid"
 	"github.com/ipfs/go-datastore"
-	blockstore "github.com/ipfs/go-ipfs-blockstore"
 	"github.com/multiformats/go-multihash"
 	"github.com/stretchr/testify/assert"
 )
